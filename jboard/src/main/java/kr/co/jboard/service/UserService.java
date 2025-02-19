ad4fbcd3dde80c1e25ada13ba60c9a3d203ce191
gyeol000726 <gyeol000726@gmail.com>
2025-02-19 14:02:00 +0900

    add all

--- jboard/src/main/java/kr/co/jboard/service/UserService.java
+++ jboard/src/main/java/kr/co/jboard/service/UserService.java
@@ -1,6 +1,16 @@
 package kr.co.jboard.service;
 
 import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import kr.co.jboard.dao.UserDAO;
 import kr.co.jboard.dto.UserDTO;
@@ -11,6 +21,10 @@ public enum UserService {
 	
 	public void registeUser(UserDTO dto) {
 		dao.insertUser(dto);
+	}
+	
+	public int countUser(String type, String value) {
+		return dao.selectCountUser(type, value);
 	}
 	
 	public UserDTO findUser(String uid) {
@@ -31,5 +45,55 @@ public enum UserService {
 	
 	public void deleteUser(String uid) {
 		dao.deleteUser(uid);
+	}
+	
+	// 이메일 발송
+	public String sendEmailCode(String receiver) {
+		
+		// 인증번호 생성
+		// 비동기 방식으로 랜덤 값 생성
+		// 백만부터 천만까지
+		int code = ThreadLocalRandom.current().nextInt(100000, 1000000);
+		
+		// Gmail 기본정보
+		// sender는 관리자가 보내는 메일
+		String sender = "gyeol000726@gmail.com";
+		String title = "jboard 인증코드입니다.";
+		String content = "<h1>인증코드는 " + code + "입니다.</h1>";
+		String appPassword = "kakq jgpj dnwi owor";
+		
+		// Gmail SMTP 서버 설정
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.ssl.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		// Gmail SMTP 세션 생성
+		Session gmailSession = Session.getInstance(prop, new Authenticator(){
+			
+			protected javax.mail.PasswordAuthentication getPasswordAuthentication(){
+				return new PasswordAuthentication(sender, appPassword);
+			}
+		});
+		
+		// 메일 발송
+		Message message = new MimeMessage(gmailSession);
+		
+		try {
+			message.setFrom(new InternetAddress(sender, "보내는 사람", "UTF-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(content, "text/html;charset=UTF-8");
+			
+			// 딜레이가 안 생기려며 스레드 처리
+			Transport.send(message);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return ""+code;
 	}
 }
