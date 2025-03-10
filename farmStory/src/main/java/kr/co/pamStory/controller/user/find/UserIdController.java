6930f9992bde33ae746cc9f270bee5f01de3ab82
gyeol000726 <gyeol000726@gmail.com>
2025-03-10 13:29:53 +0900

    add all

--- farmStory/src/main/java/kr/co/pamStory/controller/user/find/UserIdController.java
+++ farmStory/src/main/java/kr/co/pamStory/controller/user/find/UserIdController.java
@@ -8,6 +8,8 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import kr.co.pamStory.dto.UserDTO;
+import kr.co.pamStory.service.UserService;
 
 @WebServlet("/find/userId.do")
 public class UserIdController extends HttpServlet {
@@ -16,6 +18,7 @@ public class UserIdController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		
 		// View forward
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/view/find/userId.jsp");
 		dispatcher.forward(req, resp);
@@ -23,5 +26,26 @@ public class UserIdController extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		// 데이터 수신
+		String uid = req.getParameter("uid");
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		
+		// DTO 생성
+		UserDTO dto = new UserDTO();
+		dto.setUid(uid);
+		dto.setName(name);
+		dto.setEmail(email);
+		
+		if(uid != null) {
+			req.setAttribute("uid", uid);
+		}else {
+			req.setAttribute("error", "해당 정보에 일치하는 아이디가 없습니다.");
+		}
+
+		// 결과를 출력할 페이지로 포워딩
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/view/find/resultUserId.jsp");
+		dispatcher.forward(req, resp);
 	}
 }
